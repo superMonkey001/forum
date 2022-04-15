1152e3dd6767335bd4c46e69363361d0a38d7e69
superMonkey001 <3102405586@qq.com>
2022-04-15 09:24:09 +0800

    init repo

---
+++ src/test/java/cn/edu/hncj/forum/forum/ForumApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.edu.hncj.forum.forum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
