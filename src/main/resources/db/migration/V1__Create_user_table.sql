3cd06a14425915fe8d8f134199a4512eca7160a0
superMonkey001 <3102405586@qq.com>
2022-04-17 09:29:03 +0800

    add flyway migration support

---
+++ src/main/resources/db/migration/V1__Create_user_table.sql
@@ -0,0 +1,8 @@
+CREATE CACHED TABLE USER(
+    "ID" int auto_increment primary key ,
+    "ACCOUNT_ID" VARCHAR(100),
+    "NAME" VARCHAR(32),
+    "TOKEN" CHAR(36),
+    "GMT_CREATE" BIGINT,
+    "GMT_MODIFIED" BIGINT
+)
