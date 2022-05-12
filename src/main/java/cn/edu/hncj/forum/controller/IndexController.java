8d9ad23eb409f40f233d7bfd88ada3d607178c55
superMonkey001 <3102405586@qq.com>
2022-05-12 16:20:11 +0800

    完成首页展示热门标签功能

--- src/main/java/cn/edu/hncj/forum/controller/IndexController.java
+++ src/main/java/cn/edu/hncj/forum/controller/IndexController.java
@@ -1,5 +1,6 @@
 package cn.edu.hncj.forum.controller;
 
+import cn.edu.hncj.forum.cache.HotTagCache;
 import cn.edu.hncj.forum.dto.PaginationDTO;
 import cn.edu.hncj.forum.model.User;
 import cn.edu.hncj.forum.service.NotificationService;
@@ -11,18 +12,26 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 @Controller
 public class IndexController {
     @Autowired
     private QuestionService questionService;
 
+    @Autowired
+    private HotTagCache hotTagCache;
+
     @GetMapping("/")
     public String index(Model model,
                         @RequestParam(name = "page", defaultValue = "1") Integer page,
                         @RequestParam(name = "size", defaultValue = "8") Integer size,
-                        @RequestParam(name = "search", required = false) String search) {
-        PaginationDTO pagination = questionService.list(search, page, size);
+                        @RequestParam(name = "search", required = false) String search,
+                        @RequestParam(name = "hotTag", required = false) String hotTag) {
+        PaginationDTO pagination = questionService.list(search, hotTag, page, size);
+        List<String> hotTagList = hotTagCache.getHotTagList();
+        model.addAttribute("hotTags",hotTagList);
+        model.addAttribute("hotTag",hotTag);
         model.addAttribute("pagination", pagination);
         model.addAttribute("search", search);
         return "index";
