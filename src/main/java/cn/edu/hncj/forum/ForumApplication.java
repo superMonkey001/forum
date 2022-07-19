28ceab034211f382d3aa9a305eadbad613b15f46
superMonkey001 <3102405586@qq.com>
2022-07-19 23:29:48 +0800

    解决了路径中带jsessionid的问题

--- src/main/java/cn/edu/hncj/forum/ForumApplication.java
+++ src/main/java/cn/edu/hncj/forum/ForumApplication.java
@@ -3,10 +3,12 @@ package cn.edu.hncj.forum;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @MapperScan("cn.edu.hncj.forum.mapper")
+@ServletComponentScan
 @EnableScheduling
 public class ForumApplication {
 
